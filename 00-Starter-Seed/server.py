21d773e8b81e2d850b1e8bd7451fb56b9926dd3b
Osaetin Daniel <osaetindaniel@gmail.com>
2016-12-20 13:17:02 +0100

    Made code PEP-8 compliant, Removed redundant code

--- 00-Starter-Seed/server.py
+++ 00-Starter-Seed/server.py
@@ -1,7 +1,6 @@
 """Auth0's sample server
 """
 from functools import wraps
-import json
 import os
 
 from dotenv import Dotenv
@@ -27,7 +26,8 @@ app = Flask(__name__, static_url_path='')
 app.secret_key = constants.SECRET_KEY
 app.debug = True
 
-# Requires authentication annotation
+
+# Requires authentication decorator
 def requires_auth(f):
     @wraps(f)
     def decorated(*args, **kwargs):
@@ -36,40 +36,49 @@ def requires_auth(f):
         return f(*args, **kwargs)
     return decorated
 
+
 # Controllers API
 @app.route('/')
 def home():
     return render_template('home.html', env=env)
 
+
 @app.route('/dashboard')
 @requires_auth
 def dashboard():
-    return render_template('dashboard.html', user=session[constants.PROFILE_KEY])
+    return render_template('dashboard.html',
+                           user=session[constants.PROFILE_KEY])
+
 
 @app.route('/public/<path:filename>')
 def static_files(filename):
     return send_from_directory('./public', filename)
 
+
 @app.route('/callback')
 def callback_handling():
     code = request.args.get(constants.CODE_KEY)
     json_header = {constants.CONTENT_TYPE_KEY: constants.APP_JSON_KEY}
-    token_url = 'https://{auth0_domain}/oauth/token'.format(auth0_domain=env[constants.AUTH0_DOMAIN])
+    token_url = 'https://{auth0_domain}/oauth/token'.format(
+                    auth0_domain=env[constants.AUTH0_DOMAIN])
     token_payload = {
-        constants.CLIENT_ID_KEY : env[constants.AUTH0_CLIENT_ID],
-        constants.CLIENT_SECRET_KEY : env[constants.AUTH0_CLIENT_SECRET],
-        constants.REDIRECT_URI_KEY : env[constants.AUTH0_CALLBACK_URL],
-        constants.CODE_KEY : code,
-        constants.GRANT_TYPE_KEY : constants.AUTHORIZATION_CODE_KEY
+        constants.CLIENT_ID_KEY: env[constants.AUTH0_CLIENT_ID],
+        constants.CLIENT_SECRET_KEY: env[constants.AUTH0_CLIENT_SECRET],
+        constants.REDIRECT_URI_KEY: env[constants.AUTH0_CALLBACK_URL],
+        constants.CODE_KEY: code,
+        constants.GRANT_TYPE_KEY: constants.AUTHORIZATION_CODE_KEY
     }
 
-    token_info = requests.post(token_url, data=json.dumps(token_payload),
+    token_info = requests.post(token_url, json=token_payload,
                                headers=json_header).json()
-    user_url = 'https://{auth0_domain}/userinfo?access_token={access_token}'.format(
-        auth0_domain=env[constants.AUTH0_DOMAIN], access_token=token_info[constants.ACCESS_TOKEN_KEY])
+
+    user_url = 'https://{auth0_domain}/userinfo?access_token={access_token}'\
+        .format(auth0_domain=env[constants.AUTH0_DOMAIN],
+                access_token=token_info[constants.ACCESS_TOKEN_KEY])
+
     user_info = requests.get(user_url).json()
     session[constants.PROFILE_KEY] = user_info
     return redirect('/dashboard')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 3000)))
+    app.run(host='0.0.0.0', port=os.environ.get('PORT', 3000))
