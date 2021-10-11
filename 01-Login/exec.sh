b60596a075d35cdf38b0ac45effe5b21903a83ca
Carlos Valarezo <carlos.valarezo@gmail.com>
2021-10-11 10:09:47 -0500

    updates exec.sh and exec.ps1 to run the app on https

--- 01-Login/exec.sh
+++ 01-Login/exec.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 docker build -t auth0-python-web-01-login .
-docker run --env-file .env -p 3000:3000 -it auth0-python-web-01-login
+docker run --rm --env-file .env -p 10443:10443 -it auth0-python-web-01-login
